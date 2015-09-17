0b363ea20c3464316d3dc64b891a3130e101a280
Lin Jie <ljlovemichael@hotmail.com>
2015-09-17 20:13:46 +0800

    创建，添加一个匹配字符串前后的字母数字组合

---
+++ csharppractice/regexpract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regexpract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input0 = @"宽带接入技术实验室YF314";
+            string input = input0.Replace("/","");
+            string room = "";
+            if (input.Length > 12)
+            {
+                //string pattern = @"^\w*|\w*$";
+                Regex reg = new Regex("^[a-z0-9]*|[a-z0-9]*$", RegexOptions.IgnoreCase);
+                MatchCollection mc = reg.Matches(input);
+                foreach (Match item in mc)
+                {
+                    if(item.Value !="")
+                    Console.WriteLine(item.Value);
+                }
+            }
+            // return room + input;
+        }
+    }
+}
