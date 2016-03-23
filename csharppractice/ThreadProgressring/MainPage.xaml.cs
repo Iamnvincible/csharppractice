48e7930bba3e7f4c82afeaa44904a2f500f8851e
Lin Jie <ljlovemichael@hotmail.com>
2016-03-23 13:01:48 +0800

    字符编码转换

--- csharppractice/ThreadProgressring/MainPage.xaml.cs
+++ csharppractice/ThreadProgressring/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -35,25 +36,44 @@ namespace ThreadProgressring
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.txt.Text = DateTime.Now.ToString();
-            string x = "";
-            HttpClient h = new HttpClient();
-            try
+            //this.txt.Text = DateTime.Now.ToString();
+            //string x = "";
+            //HttpClient h = new HttpClient();
+            //try
+            //{
+            //    var response = h.GetBufferAsync(new Uri("http://202.202.43.125/"));
+
+            //    x = await h.GetStringAsync(new Uri("http://202.202.43.125/"));
+            //}
+            //catch (Exception xx)
+            //{
+            //    Debug.WriteLine(xx.Message);
+            //}
+            //Debug.WriteLine(x);
+            //Debug.WriteLine("-=--------------------------------------------------------");
+            ////异步执行并得到结果
+            //x = await getstring(new Uri("http://202.202.43.125/"));
+            ////异步赋值在执行
+            //Task<string> rrrrr = getstring(new Uri("http://202.202.43.125/"));
+            //var c = await rrrrr;
+            //Debug.WriteLine(x);
+            HttpClient httpclient = new HttpClient();
+            var uri = new Uri("http://apis.baidu.com/songshuxiansheng/real_time/search_news?keyword=明星");
+            httpclient.DefaultRequestHeaders.Add("apikey", "9a84555d8b243d4afa83cac9855b60e7");
+            var response = await httpclient.GetAsync(uri);
+            var r2 = await httpclient.GetStringAsync(uri);
+            var r3 = await httpclient.GetBufferAsync(uri);
+            var r33 = r3.ToArray();
+            string result = Encoding.UTF8.GetString(r33);
+            char[] aaaa =
             {
-                x = await h.GetStringAsync(new Uri("http://202.202.43.125/"));
-            }
-            catch (Exception xx)
+               (Char)0x4ed6,(Char)0x662f,(Char)0x5a31,(Char)0x4e50,(Char)0x5708,(Char)0x91cc,(Char)0x6700,(Char)0x5bd2,(Char)0x9178,(Char)0x7684,(Char)0x660e,(Char)0x661f,(Char)0xff0c,(Char)0x5374,(Char)0x503c,(Char)0x5f97,(Char)0x6bcf,(Char)0x4e00,(Char)0x4e2a,(Char)0x4eba,(Char)0x5c0a,(Char)0x91cd,(Char)0xff01
+            };
+            foreach (var VARIABLE in aaaa)
             {
-                Debug.WriteLine(xx.Message);
+                Debug.Write(VARIABLE);
             }
-            Debug.WriteLine(x);
-            Debug.WriteLine("-=--------------------------------------------------------");
-            //异步执行并得到结果
-            x = await getstring(new Uri("http://202.202.43.125/"));
-            //异步赋值在执行
-            Task<string> rrrrr = getstring(new Uri("http://202.202.43.125/"));
-            var c = await rrrrr;
-            Debug.WriteLine(x);
+            //  textblock.Text = await response.Content.ReadAsStringAsync();
         }
 
         private async void longtime(object sender, RoutedEventArgs e)
