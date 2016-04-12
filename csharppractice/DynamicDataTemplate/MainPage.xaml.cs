0b8ea741e1e3836b3e5baf073f7a3ad4f2034234
Lin Jie <ljlovemichael@hotmail.com>
2016-04-12 23:38:05 +0800

    删了点多余代码

--- csharppractice/DynamicDataTemplate/MainPage.xaml.cs
+++ csharppractice/DynamicDataTemplate/MainPage.xaml.cs
@@ -36,54 +36,5 @@ namespace DynamicDataTemplate
             this.InitializeComponent();
             ViewModel = new MainViewModel();
         }
-
-        //private async Task getdata()
-        //{
-        //    string jsonstr = await GetJsonFromFile();
-        //    JsonObject jo = JsonObject.Parse(jsonstr);
-        //    var status = jo["status"].ToString();
-        //    if (status == "200")
-        //    {
-        //        foreach (var item in jo.GetNamedArray("data", new JsonArray()))
-        //        {
-        //            if (item.ValueType == JsonValueType.Object)
-        //            {
-        //                string rs = item.Stringify();
-        //                byte[] array = Encoding.UTF8.GetBytes(rs);
-        //                MemoryStream stream = new MemoryStream(array);
-        //                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(News));
-        //                var s = (News)ser.ReadObject(stream);
-        //                list.Add(s);
-        //            }
-        //        }
-        //    }
-        //    this.newslist.ItemsSource = list;
-        //}
-        //private async Task<string> GetJsonFromFile()
-        //{
-        //    //设置文件的目录，在项目下的文件用ms-appx:///开始目录
-        //    Uri datauri = new Uri("ms-appx:///SampleData.json");
-        //    //文件读写可能会有异常
-        //    try
-        //    {
-        //        //获取文件
-        //        StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(datauri);
-        //        //读取文件内容
-        //        string jsonstring = await FileIO.ReadTextAsync(file);
-        //        //返回文件内容
-        //        return jsonstring;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        //打印异常信息
-        //        Debug.WriteLine(e.Message);
-        //    }
-        //    return null;
-        //}
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-         //   await getdata();
-        }
     }
 }
