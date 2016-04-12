47409b65aa56549060e8d4190802aa346fb15407
Lin Jie <ljlovemichael@hotmail.com>
2016-04-12 10:10:44 +0800

    Frame

---
+++ csharppractice/FrameUWPStudy/BlankPage2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FrameUWPStudy
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage2 : Page
+    {
+        public BlankPage2()
+        {
+            this.InitializeComponent();
+            SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManager_BackRequseted;
+
+        }
+
+        private void SystemNavigationManager_BackRequseted(object sender, BackRequestedEventArgs e)
+        {
+            Debug.WriteLine("page2");
+        }
+    }
+}
