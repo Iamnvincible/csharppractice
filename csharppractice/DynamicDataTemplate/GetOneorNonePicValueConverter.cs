fd2ea9b91c4f726a589accb46e0a2b0344c62315
Lin Jie <ljlovemichael@hotmail.com>
2016-05-05 16:17:39 +0800

    我的消息完成

---
+++ csharppractice/DynamicDataTemplate/GetOneorNonePicValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace DynamicDataTemplate
+{
+    public class GetOneorNonePicValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string src = value as string;
+            if (src == "")
+            {
+                return "";
+            }
+            else if (src.Split(',').Length > 1)
+            {
+                var s = src.Split(',')[0];
+                return s;
+            }
+            else
+            {
+                return src;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
