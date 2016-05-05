bae6737f09bca1717b11085a48133b9ea5b951e1
Lin Jie <ljlovemichael@hotmail.com>
2016-05-05 18:31:53 +0800

    我的动态开头。

---
+++ csharppractice/DynamicDataTemplate/Data/MyFeeds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDataTemplate.Data
+{
+    public class MyFeeds
+    {
+
+
+            public string id { get; set; }
+            public string photo_src { get; set; }
+            public string thumbnail_src { get; set; }
+            public string content { get; set; }
+            public string type_id { get; set; }
+            public string created_time { get; set; }
+            public string updated_time { get; set; }
+            public string like_num { get; set; }
+            public string remark_num { get; set; }
+        
+
+    }
+}
